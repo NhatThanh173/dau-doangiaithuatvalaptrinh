875d86e2710070ddbc19954530679800d5280e5b
NhatThanh173 <thanhanhok1@gmail.com>
2026-03-06 10:47:04 +0700

    DONE

---
+++ bai23_454.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        
+        unordered_map<int,int> demTong;
+        
+        for(int i = 0; i < nums1.size(); i++){
+            for(int j = 0; j < nums2.size(); j++){
+                int tong = nums1[i] + nums2[j];
+                demTong[tong]++;
+            }
+        }
+
+        int ketQua = 0;
+
+        for(int i = 0; i < nums3.size(); i++){
+            for(int j = 0; j < nums4.size(); j++){
+                
+                int tong = nums3[i] + nums4[j];
+                int canTim = -tong;
+
+                if(demTong.count(canTim)){
+                    ketQua += demTong[canTim];
+                }
+            }
+        }
+
+        return ketQua;
+    }
+};
