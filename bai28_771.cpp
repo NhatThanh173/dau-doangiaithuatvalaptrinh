6d6f51cbfdbba9d91c72e6c588829876993d0203
NhatThanh173 <thanhanhok1@gmail.com>
2026-03-06 10:51:07 +0700

    DONE

---
+++ bai28_771.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count = 0;
+
+        for(char s : stones){
+            for(char j : jewels){
+                if(s == j){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
