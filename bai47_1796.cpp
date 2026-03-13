6981cd46b25de56e39e1c2ce294d704bbd5372da
NhatThanh173 <thanhanhok1@gmail.com>
2026-03-13 10:38:13 +0700

    DONE

---
+++ bai47_1796.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int secondHighest(string s) {
+        int max1 = -1;
+        int max2 = -1;
+
+        for(char c : s){
+            if(isdigit(c)){
+                int num = c - '0';
+
+                if(num > max1){
+                    max2 = max1;
+                    max1 = num;
+                }
+                else if(num > max2 && num != max1){
+                    max2 = num;
+                }
+            }
+        }
+
+        return max2;
+    }
+};
