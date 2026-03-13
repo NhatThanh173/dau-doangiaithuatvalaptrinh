a72a214392438315763d09a10fab6f20259ce6ac
NhatThanh173 <thanhanhok1@gmail.com>
2026-03-13 10:34:34 +0700

    DONE

---
+++ bai45_1748.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int freq[101] = {0};
+
+        for(int num : nums){
+            freq[num]++;
+        }
+
+        int sum = 0;
+
+        for(int num : nums){
+            if(freq[num] == 1)
+                sum += num;
+        }
+
+        return sum;
+    }
+};
