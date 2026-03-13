014ab1a9fb146d919a4f160f6f16f238055ee5c1
NhatThanh173 <thanhanhok1@gmail.com>
2026-03-13 10:20:33 +0700

    DONE

---
+++ bai42_1694.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reformatNumber(string number) {
+        string digits = "";
+
+        // Bước 1: lấy chỉ các chữ số
+        for(char c : number){
+            if(isdigit(c))
+                digits += c;
+        }
+
+        string res = "";
+        int i = 0;
+        int n = digits.size();
+
+        while(n - i > 4){
+            res += digits.substr(i,3) + "-";
+            i += 3;
+        }
+
+        if(n - i == 4){
+            res += digits.substr(i,2) + "-";
+            res += digits.substr(i+2,2);
+        }
+        else{
+            res += digits.substr(i);
+        }
+
+        return res;
+    }
+};
